66abe14f1008a7649ec63336079d70c0c2aa3033
fretlesskey <jmsnavarro@gmail.com>
2018-08-13 18:55:24 +0800

    Code cleanup

--- fromExcelToCSV_py243.py
+++ fromExcelToCSV_py243.py
@@ -3,26 +3,27 @@
 
 """ Tool to Convert Excel file to CSV
 # fromExcelToCSV_py243.py
-A terminal script written in Python that converts Excel files to CSV.
-Developed using Visual Studio Code with Python extension.
-Based from https://dzone.com/articles/using-python-to-extract-excel-spreadsheet-into-csv
+A terminal script written in Python that reads Excel rows and export 
+to a pipe-delimited csv file.
 
-# This script targets python 2.4.3
+Developed using Visual Studio Code with Python extension.
+
+This script targets python 2.4.3.
 
 ## Source file requirements
-1. Will only accept *.xls Excel files. Not *.xlsx.
+1. Will only accept *.xls Excel files and not *.xlsx.
 2. Source must be placed on the same directory as the python script
-3. To modify default filename, update src_filename variable
+3. To modify default filename, update SRC_FILENAME variable
 4. First sheet name must be a month name (e.g. January)
-5. Year must be placed at 'D3' cell (e.g. 2018)
-6. Type of flight must be placed at 'C' cell on any row (e.g. International)
-7. Data rows are read from 'A' to 'F' cells where 'A' cell values must be numeric
+5. Year must be placed at 'E3' cell (e.g. 2018)
+6. Type of menu must be placed at 'A' cell on any row (e.g. International)
+7. Data rows are read from 'A' to 'E' cells where 'A' cell values must be numeric
 
 ## How to run:
 $ python fromExcelToCSV_py243.py
 
 ## Output: csv and log files
-YYYYMMsrc_filename.csv
+YYYYMMSRC_FILENAME.csv
 (e.g. 201804FOOD_MENU.csv)
 
 YYYYMMDD_HHMMHH_exceltocsv.py.log
@@ -48,8 +49,8 @@ import xlrd
 
 
 # format month name
-def getnumericmonth(monthname):
-    month = monthname.upper()
+def getNumMonthVal(monthName):
+    month = monthName.upper()
     if month == 'JAN' or month == 'JANUARY':
         return '01'
     elif month == 'FEB' or month == 'FEBRUARY':
@@ -80,59 +81,59 @@ def getnumericmonth(monthname):
 
 # main definition
 def main():
-    src_filename = 'food_menu.xls'
-    f = glob.glob(os.path.join(src_filename))
+    SRC_FILENAME = 'food_menu.xls'
+    f = glob.glob(os.path.join(SRC_FILENAME))
 
     if not f:
-        msg = 'Nothing to process. Cannot find %s file.' % src_filename
+        msg = 'Nothing to process. Cannot find %s file.' % SRC_FILENAME
         print('%s: %s\n' % (logging.info.__name__.upper(), msg))
         logging.info(msg)
         sys.exit()
     else:
-        wb = xlrd.open_workbook(src_filename)
+        wb = xlrd.open_workbook(SRC_FILENAME)
         sheet = wb.sheet_by_index(0)
-        reportyear = int(sheet.cell_value(2, 3))
-        reportmonth = getnumericmonth(sheet.name)
-        if reportmonth == '00':
+        reportYear = int(sheet.cell_value(2, 4))
+        reportMonth = getNumMonthVal(sheet.name)
+        if reportMonth == '00':
             msg = 'Undefined month as \'%s\'. Please check source file.' % sheet.name
             print('%s: %s\n' % (logging.warning.__name__.upper(), msg))
             logging.warning(msg)
             sys.exit()
         else:
-            reportmonthname = sheet.name
+            reportMonthName = sheet.name
 
-        msg = 'Reading %s for %s %s' % (src_filename, reportmonthname.title(), reportyear)
+        msg = 'Reading %s for %s %s' % (SRC_FILENAME, reportMonthName.title(), reportYear)
         print('%s: %s' % (logging.info.__name__.upper(), msg))
         logging.info(msg)
 
-        out_filename = '%s%s%s.csv' % (reportyear, reportmonth, os.path.splitext(src_filename)[0])
+        OUT_FILENAME = '%s%s%s.csv' % (reportYear, reportMonth, os.path.splitext(SRC_FILENAME)[0])
 
-        f = open(out_filename, "wb")
+        f = open(OUT_FILENAME, "wb")
         try:
             writer = csv.writer(f, delimiter="|")
 
-            typeflight = ''
+            typeMenu = ''
             row = []
             rowcount = 0
-            for row_idx in range(sheet.nrows):
-                # get flight type
-                if not isinstance(sheet.cell_value(row_idx, 2), str):
-                    if sheet.cell_value(row_idx, 2).upper() == 'DOMESTIC':
-                        typeflight = 'DOMESTIC'
-                    elif sheet.cell_value(row_idx, 2).upper() == 'INTERNATIONAL':
-                        typeflight = 'INTERNATIONAL'
-                # fetching data
-                if isinstance(sheet.cell_value(row_idx, 0), (int, float)):
-                    for col_idx in range(1, 6):
-                        row.insert(col_idx, sheet.cell_value(row_idx, col_idx))
+            for row_id in range(sheet.nrows):
+                # get type of menu
+                if not isinstance(sheet.cell_value(row_id, 0), str):
+                    if sheet.cell_value(row_id, 0).upper() == 'PLATTER':
+                        typeMenu = 'PLATTER'
+                    elif sheet.cell_value(row_id, 0).upper() == 'DRINKS':
+                            typeMenu = 'DRINKS'
+                # collect data and write to file
+                if isinstance(sheet.cell_value(row_id, 0), (int, float)):
+                    for col_id in range(1, 5):
+                        row.insert(col_id, sheet.cell_value(row_id, col_id))
                     row.insert(0, sheet.name[0:3].upper())
-                    row.insert(1, reportyear)
-                    row.insert(2, typeflight)
+                    row.insert(1, reportYear)
+                    row.insert(2, typeMenu)
                     writer.writerow(row)
 
                     rowcount += 1
 
-                    msg = 'Fetching \'%s\' data from row %s' % (typeflight.title(), row_idx)
+                    msg = 'Collecting data from \'%s\' at row %s' % (typeMenu.title(), row_id)
                     print('%s: %s' % (logging.info.__name__.upper(), msg))
                     logging.info(msg)
                 row = []
@@ -141,23 +142,23 @@ def main():
 
         # summary
         if rowcount > 0:
-            msg = 'Done copying %s row(s) to %s' % (rowcount, out_filename)
+            msg = 'Done copying %s row(s) to %s' % (rowcount, OUT_FILENAME)
         else:
-            msg = 'No rows to fetch.'
+            msg = 'No rows to collect.'
 
-        elapsedtime = timedelta(seconds=round(time.time() - starttime_main))
-        print('\nSUMMARY: %s | Elapsed time %s\n' % (msg, elapsedtime))
-        logging.info('%s | Elapsed time %s' % (msg, elapsedtime))
+        elapsedTime = timedelta(seconds=round(time.time() - startTime_Main))
+        print('\nSUMMARY: %s | Elapsed time %s\n' % (msg, elapsedTime))
+        logging.info('%s | Elapsed time %s' % (msg, elapsedTime))
 
 
 # main
 if __name__ == "__main__":
     # set runtime
-    starttime_main = time.time()
+    startTime_Main = time.time()
 
     # set global logging
-    logfilename = '%s %s.log' % (time.strftime('%Y%m%d_%H%M%S'), os.path.basename(__file__))
-    logging.basicConfig(filename=os.path.join(logfilename),
+    logFilename = '%s %s.log' % (time.strftime('%Y%m%d_%H%M%S'), os.path.basename(__file__))
+    logging.basicConfig(filename=os.path.join(logFilename),
                         level=logging.DEBUG,
                         format="[%(levelname)s] : %(asctime)s : %(message)s")
 
